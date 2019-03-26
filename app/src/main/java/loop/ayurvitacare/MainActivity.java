b72715783224cc37c61c5de2552ffa8a64850638
lalitsingh <lalitbisalpur@gmail.com>
2019-03-26 12:39:18 +0530

    first

---
+++ app/src/main/java/loop/ayurvitacare/MainActivity.java
@@ -0,0 +1,28 @@
+package loop.ayurvitacare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = findViewById(R.id.webview);
+        webView.getSettings().getJavaScriptEnabled();
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.getSettings().setPluginState(WebSettings.PluginState.ON);
+        //  webView.getSettings().setUserAgentString(userAgent);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setDatabaseEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.loadUrl("http://ayurvitacare.com/index.html");
+    }
+}
